758787454f08dafcffa75aada402a975299300f2
k-onbfans <809103605@qq.com>
2019-09-25 13:56:21 +0800

    Complete Joseph and Fibonacci

--- Joseph.js
+++ Joseph.js
@@ -7,16 +7,16 @@ router.use(bodyParser.urlencoded({ extended: true }));
 router.post('/',(req,res) => {
     var list = req.body.elements;
     var num = req.body.startIndex;
-    list.remove(num);
+    list.splice(num,1);
     while(list.length > 1){
         num = num + req.body.interval - 1;
         if(num > list.length-1){
             while(num > list.length-1){
                 num = num - list.length;
             }
-            list.remove(num);
+            list.splice(num,1);
         }else{
-            list.remove(num);
+            list.splice(num,1);
         }
     }
     res.send(list[0])
